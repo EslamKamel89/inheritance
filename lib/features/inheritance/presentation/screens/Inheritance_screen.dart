61c57c17a7b9fabdb19ad0979cca56ee891902c6
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-05 14:42:06 +0200

    Add Inheritance screen with text input widget and navigation controls

--- lib/features/inheritance/presentation/screens/Inheritance_screen.dart
+++ lib/features/inheritance/presentation/screens/Inheritance_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:inheritance/core/extensions/context-extensions.dart';
 import 'package:inheritance/core/widgets/main_scaffold.dart';
+import 'package:inheritance/utils/styles/styles.dart';
 
 class InheritanceScreen extends StatefulWidget {
   const InheritanceScreen({super.key});
@@ -11,6 +13,64 @@ class InheritanceScreen extends StatefulWidget {
 class _InheritanceScreenState extends State<InheritanceScreen> {
   @override
   Widget build(BuildContext context) {
-    return MainScaffold(appBarTitle: "Bismillah, Let's Begin", child: SizedBox());
+    return MainScaffold(
+      appBarTitle: "Bismillah, Let's Begin",
+      child: SingleChildScrollView(child: Column(children: [
+        
+      ],)),
+    );
+  }
+}
+
+class InheritanceTextInputWidget extends StatefulWidget {
+  const InheritanceTextInputWidget({
+    super.key,
+    required this.image,
+    this.label,
+    required this.updateInput,
+    this.handleSubmit,
+    this.handleBack,
+    this.backTitle,
+    this.nextTitle,
+  });
+  final String? image;
+  final String? label;
+  final void Function(String val) updateInput;
+  final void Function()? handleSubmit;
+  final void Function()? handleBack;
+  final String? backTitle;
+  final String? nextTitle;
+  @override
+  State<InheritanceTextInputWidget> createState() => _InheritanceTextInputWidgetState();
+}
+
+class _InheritanceTextInputWidgetState extends State<InheritanceTextInputWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (widget.label != null) txt(widget.label!, e: St.bold20),
+        TextFormField(),
+        Row(
+          children: [
+            if (widget.handleBack != null)
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(color: context.primaryColor),
+                  child: txt('Back'),
+                ),
+              ),
+            if (widget.handleSubmit != null)
+              InkWell(
+                child: Container(
+                  decoration: BoxDecoration(color: context.primaryColor),
+                  child: txt('Next'),
+                ),
+              ),
+          ],
+        ),
+      ],
+    );
   }
 }
